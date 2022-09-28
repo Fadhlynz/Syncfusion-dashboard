123337fa71b60ed01b1bf3964cfe663a5746dd51
Fadli Nur Zaman <fadhlynz@gmail.com>
2022-09-28 12:33:22 +0800

    Last Update Navbar

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import { FiSettings } from "react-icons/fi";
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 
 import { Navbar, Footer, Sidebar, ThemeSettings } from "./components";
-import { Ecommerce, Orders, Employees, Customers, Calendar, Stacked, Pyramid, Kanban, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor , Line } from "./pages";
+import { Ecommerce, Orders, Employees, Customers, Calendar, Stacked, Pyramid, Kanban, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor, Line } from "./pages";
+
+import { useStateContext } from "./contexts/ContextProvider";
 
 import "./App.css";
 
 const App = () => {
-  const activeMenu = true;
+  const { activeMenu } = useStateContext(); 
   return (
     <div>
       <BrowserRouter>
