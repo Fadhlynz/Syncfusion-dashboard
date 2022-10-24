82f93dfb1483808954dac08d6276d1c320dae2c0
Fadli Nur Zaman <fadhlynz@gmail.com>
2022-10-24 15:12:13 +0800

    updated

--- src/pages/ECommerce.jsx
+++ src/pages/ECommerce.jsx
@@ -3,7 +3,11 @@ import { BsCurrencyDollar } from "react-icons/bs";
 import { GoPrimitiveDot } from "react-icons/go";
 
 import { Stacked, Pie, Button, SparkLine } from "../components";
-import { earningData, SparklineAreaData, ecomPieChartData } from "../data/dummy";
+import {
+  earningData,
+  SparklineAreaData,
+  ecomPieChartData,
+} from "../data/dummy";
 import { useStateContext } from "../contexts/ContextProvider";
 
 const Ecommerce = () => {
@@ -46,9 +50,7 @@ const Ecommerce = () => {
                   {item.percentage}
                 </span>
               </p>
-              <p className="text-sm text-gray-400 mt-1">
-                {item.title}
-              </p>
+              <p className="text-sm text-gray-400 mt-1">{item.title}</p>
             </div>
           ))}
         </div>
@@ -57,33 +59,64 @@ const Ecommerce = () => {
       <div className="flex gap-10 flex-wrap justify-center">
         <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg m-3 p-4 rounded-2xl md:w-780">
           <div className="flex justify-between">
-            <p className="font-semibold text-xl">
-              Revenue Updates
-            </p>
+            <p className="font-semibold text-xl">Revenue Updates</p>
             <div className="flex items-center gap-4">
               <p className="flex items-center gap-2 text-gray-600 hover:drop-shadow-xl">
                 <span>
                   <GoPrimitiveDot />
                 </span>
-                <span>
-                  Expense
-                </span>
+                <span>Expense</span>
               </p>
               <p className="flex items-center gap-2 text-green-400 hover:drop-shadow-xl">
                 <span>
                   <GoPrimitiveDot />
                 </span>
-                <span>
-                  Budget
-                </span>
+                <span>Budget</span>
               </p>
-
             </div>
-
           </div>
-
+          <div className="mt-10 flex gap-10 flex-wrap justify-center">
+            <div className="border-r-1 border-color m-4 pr-10">
+              <div>
+                <p>
+                  <span className="text-3xl font-semibold ">$93,438</span>
+                  <span className="p-1.5 hover:drop-shadow-xl cursor-pointer rounded-full text-white bg-green-400 ml-3 text-xs">
+                    23%
+                  </span>
+                </p>
+                <p className="text-gray-500 mt-1">Budget</p>
+              </div>
+              <div className="mt-8">
+                <p>
+                  <span className="text-3xl font-semibold ">$48,438</span>
+                </p>
+                <p className="text-gray-500 mt-1">Expense</p>
+              </div>
+              <div className="mt-5">
+                <SparkLine
+                  currentColor="blue"
+                  id="line-sparkline"
+                  type="Line"
+                  height="80px"
+                  width="250px"
+                  data={SparklineAreaData}
+                  color="blue"
+                />
+              </div>
+              <div className="mt-10">
+                <Button
+                  color="white"
+                  bgColor="blue"
+                  text="Download Report"
+                  borderRadius="10px"
+                />
+              </div>
+            </div>
+            <div>
+              <Stacked width="320px" height="360px" />
+            </div>
+          </div>
         </div>
-
       </div>
     </div>
   );
